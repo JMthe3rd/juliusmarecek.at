77f95b311a1d51da55bfed23853064ca539998f9
JMthe3rd <jules.marecek@gmail.com>
2023-08-14 13:36:52 +0200

    insert slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marecekjulius.at';
+export class AppComponent implements OnInit {
+  images = ['augarten-palace.jpg', 'htl.jpg', 'pan.jpg', 'student.jpg', 'laptop.jpg'];
+  currentImage = 0;
+  showImage =  true;
 
+  ngOnInit() {
+    this.updateImage();
+  }
 
+  updateImage() {
+    setInterval(() => {
+        this.currentImage++;
+        this.currentImage = this.currentImage % this.images.length;
+        this.showImage = false;
 
-  
+        setTimeout(() => {
+          this.showImage = true;
+        },10);
+    },8000);
+  }
 }
